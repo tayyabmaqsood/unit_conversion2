41415c071fb0be7fa7880d288167a1888b47b1bb
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-11-02 00:43:53 +0500

    Add simple button action

--- app/src/main/java/com/example/unitconversion/MainActivity.java
+++ app/src/main/java/com/example/unitconversion/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.unitconversion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button resultButton = (Button) findViewById(R.id.ResultButton);
+        TextView resultView = (TextView) findViewById(R.id.ResultText);
+
+        resultButton.setOnClickListener(v -> {
+            resultView.setText("Button is pressed");
+        });
     }
 }
