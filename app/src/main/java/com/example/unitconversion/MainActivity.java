d6c800f2ce54d19e83fdf0892f78431f790026d5
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-11-02 02:34:38 +0500

    Complete functionality added

--- app/src/main/java/com/example/unitconversion/MainActivity.java
+++ app/src/main/java/com/example/unitconversion/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -14,21 +15,37 @@ import javax.xml.transform.Result;
 public class MainActivity extends AppCompatActivity {
     RadioGroup radioGroup;
     RadioButton radioButton;
+    TextView inputText,resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         radioGroup = findViewById(R.id.radioGroup);
+        inputText = findViewById(R.id.InputNumberText);
+        resultText = findViewById(R.id.ResultText);
 
-//        Button resultButton = (Button) findViewById(R.id.ResultButton);
-//        TextView resultView = (TextView) findViewById(R.id.ResultText);
-//
-//        resultButton.setOnClickListener(v -> {
-//            resultView.setText("Button is pressed");
-//        });
     }
     public void checkButton(View V){
-    int radioId = radioGroup.getCheckedRadioButtonId();     //Will give us ID of radio button that we checked
-    radioButton = findViewById(radioId);
+        int radioId = radioGroup.getCheckedRadioButtonId();     //Will give us ID of radio button that we checked
+        radioButton = (RadioButton) findViewById(radioId);
+    }
+
+    public void calculate(View V){
+        if (radioButton != null) {
+            if (radioButton.getText().toString().equals("Kilo to Pound Convertor") && inputText.getText().toString().length() != 0) {
+                Double result = Double.parseDouble(inputText.getText().toString()) * 2.20462;
+                resultText.setText(result.toString() + " Pounds");
+            }
+            else if (radioButton.getText().toString().equals( "Pound to Kilo Convertor") && inputText.getText().toString().length() != 0)
+            {
+                Double result = Double.parseDouble(inputText.getText().toString()) / 0.454;
+                resultText.setText(result.toString() + " Kilos");
+            }
+            else
+                resultText.setText("Please make sure you enter provide input");
+
+        }
+        else
+            resultText.setText("Please make sure you select convertor option");
     }
 }
