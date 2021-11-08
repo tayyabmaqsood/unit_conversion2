b2b83ec1dfafc4bc52b595786abcbc5b37d7e95d
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-11-09 00:36:39 +0500

    new activity created

---
+++ app/src/main/java/com/example/unitconversion/share.java
@@ -0,0 +1,13 @@
+package com.example.unitconversion;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class share extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
